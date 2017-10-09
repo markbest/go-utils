94ec79f3f36db424fb34c86c406bb52e7c2da924
mafengtao <mafengtao@yfcloud.com>
2017-10-09 18:45:37 +0800

    initialize code

---
+++ elastic.go
@@ -0,0 +1,247 @@
+package go_utils
+
+import (
+	"gopkg.in/olivere/elastic.v3"
+	"log"
+)
+
+type ES struct {
+	es_host      string
+	es_index     string
+	es_Type      string
+	logHandle    *log.Logger
+	ES_Client    *elastic.Client
+	bool_query   *elastic.BoolQuery
+	aggregations map[string]elastic.Aggregation
+	size         int
+	form         int
+	sort         map[string]bool
+}
+
+// FinderResponse is the outcome of calling Finder.Find.
+type ESResponse struct {
+	Total  int64
+	Result []interface{}
+	Aggs   elastic.Aggregations
+}
+
+//New ES
+func NewES(host string, logHandle *log.Logger) *ES {
+	//retry
+	retry := elastic.NewBackoffRetrier(elastic.ZeroBackoff{})
+
+	//connect
+	client, err := elastic.NewClient(
+		elastic.SetURL(host),
+		elastic.SetSniff(false),
+		elastic.SetInfoLog(logHandle),
+		elastic.SetRetrier(retry),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return &ES{
+		es_host:      host,
+		logHandle:    logHandle,
+		ES_Client:    client,
+		bool_query:   elastic.NewBoolQuery(),
+		aggregations: make(map[string]elastic.Aggregation),
+		size:         10000,
+		form:         0,
+		sort:         make(map[string]bool),
+	}
+}
+
+//Set ES Index
+func (e *ES) Index(es_index string) *ES {
+	e.es_index = es_index
+	return e
+}
+
+//Set ES Type
+func (e *ES) Type(es_type string) *ES {
+	e.es_Type = es_type
+	return e
+}
+
+//ES Where Condition search
+func (e *ES) Where(field string, value interface{}) *ES {
+	e.bool_query = e.bool_query.Must(elastic.NewTermQuery(field, value))
+	return e
+}
+
+//Set Size
+func (e *ES) Take(size int) *ES {
+	e.size = size
+	return e
+}
+
+//Set Search From
+func (e *ES) Page(page int) *ES {
+	e.form = e.size * (page - 1)
+	return e
+}
+
+//ES Range Condition search
+func (e *ES) Range(field string, values map[string]interface{}) *ES {
+	range_query := elastic.NewRangeQuery(field)
+	for k, v := range values {
+		if k == "gt" {
+			range_query.Gt(v)
+		}
+		if k == "gte" {
+			range_query.Gte(v)
+		}
+		if k == "lt" {
+			range_query.Lt(v)
+		}
+		if k == "lte" {
+			range_query.Lte(v)
+		}
+	}
+	e.bool_query = e.bool_query.Must(range_query)
+	return e
+}
+
+//ES Avg Aggregation
+func (e *ES) Avg(field string) *ES {
+	avg := elastic.NewAvgAggregation().Field(field)
+	e.aggregations["avg"] = avg
+	return e
+}
+
+//ES Max Aggregation
+func (e *ES) Max(field string) *ES {
+	max := elastic.NewMaxAggregation().Field(field)
+	e.aggregations["max"] = max
+	return e
+}
+
+//ES Min Aggregation
+func (e *ES) Min(field string) *ES {
+	min := elastic.NewMinAggregation().Field(field)
+	e.aggregations["min"] = min
+	return e
+}
+
+//ES Sum Aggregation
+func (e *ES) Sum(field string) *ES {
+	sum := elastic.NewSumAggregation().Field(field)
+	e.aggregations["sum"] = sum
+	return e
+}
+
+//ES Count Aggregation
+func (e *ES) Count(field string) *ES {
+	count := elastic.NewValueCountAggregation().Field(field)
+	e.aggregations["count"] = count
+	return e
+}
+
+//ES GroupBy Aggregation
+func (e *ES) GroupBy(field string) *ES {
+	groupBy := elastic.NewTermsAggregation().Field(field).Size(e.size)
+	e.aggregations["groupBy"] = groupBy
+	return e
+}
+
+//ES OrderBy Query
+func (e *ES) OrderBy(field string, order string) *ES {
+	var dir bool
+	if order == "desc" {
+		dir = false
+	} else {
+		dir = true
+	}
+	e.sort[field] = dir
+	return e
+}
+
+//ES Create Index
+func (e *ES) CreateIndex(index string) {
+	exists, err := e.ES_Client.IndexExists(index).Do()
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		createIndex, err := e.ES_Client.CreateIndex(index).Do()
+		if err != nil {
+			panic(err)
+		}
+		if !createIndex.Acknowledged {
+			panic("create index failed!")
+		}
+	}
+}
+
+//ES Delete Index
+func (e *ES) DeleteIndex(index string) {
+	exists, err := e.ES_Client.IndexExists(index).Do()
+	if err != nil {
+		panic(err)
+	}
+	if exists {
+		_, err = e.ES_Client.DeleteIndex(index).Do()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		panic("index: " + index + " not exist.")
+	}
+}
+
+//ES Get Response
+func (e *ES) Search() (ESResponse, error) {
+	var resp ESResponse
+
+	// Create service and use query, aggregations, filter
+	search := e.ES_Client.Search().
+		Index(e.es_index).
+		Type(e.es_Type).
+		Query(e.bool_query).
+		Size(e.size).
+		From(e.form)
+
+	// Add aggregation
+	if len(e.aggregations) > 0 {
+		for k, v := range e.aggregations {
+			search = search.Aggregation(k, v)
+		}
+	}
+
+	// Add SortBy
+	if len(e.sort) > 0 {
+		for k, v := range e.sort {
+			search = search.Sort(k, v)
+		}
+	}
+
+	// Execute query
+	sr, err := search.Do()
+	if err != nil {
+		panic(err)
+	}
+
+	// Decode response
+	rs, aggs, err := e.decodeResponse(sr)
+	if err != nil {
+		return resp, err
+	}
+	resp.Result = rs
+	resp.Total = sr.Hits.TotalHits
+	resp.Aggs = aggs
+	return resp, nil
+}
+
+// DecodeLogs takes a search result and deserializes the response.
+func (e *ES) decodeResponse(res *elastic.SearchResult) ([]interface{}, elastic.Aggregations, error) {
+	if res == nil || res.TotalHits() == 0 {
+		return nil, nil, nil
+	}
+
+	var rss []interface{}
+	for _, hit := range res.Hits.Hits {
+		rss = append(rss, *hit.Source)
+	}
+	return rss, res.Aggregations, nil
+}
---
+++ redis.go
@@ -0,0 +1,74 @@
+package go_utils
+
+import (
+	"github.com/garyburd/redigo/redis"
+)
+
+type cacheRedis struct {
+	client redis.Conn
+}
+
+//New redis conn
+func NewRedis(host string, port string, db int, password string) *cacheRedis {
+	c, err := redis.Dial("tcp", host+":"+port)
+	c.Do("SELECT", db)
+	c.Do("AUTH", password)
+	if err != nil {
+		panic(err)
+	}
+	r := &cacheRedis{client: c}
+	return r
+}
+
+//Set string
+func (c *cacheRedis) set(key string, value string, expire int64) (err error) {
+	_, err = c.client.Do("SET", key, value)
+	if expire > 0 {
+		c.client.Do("EXPIRE", key, expire)
+	}
+	return err
+}
+
+//Get string
+func (c *cacheRedis) get(key string) (value string, err error) {
+	value, err = redis.String(c.client.Do("GET", key))
+	return value, err
+}
+
+//Set hash
+func (c *cacheRedis) hSet(key string, value interface{}, expire int64) (err error) {
+	_, err = c.client.Do("HMSET", redis.Args{}.Add(key).AddFlat(value)...)
+	if expire > 0 {
+		c.client.Do("EXPIRE", key, expire)
+	}
+	return err
+}
+
+//Get hash
+func (c *cacheRedis) hGet(key string, value interface{}) (err error) {
+	v, err := redis.Values(c.client.Do("HGETALL", key))
+	if err != nil {
+		return err
+	}
+	if err := redis.ScanStruct(v, value); err != nil {
+		return err
+	}
+	return err
+}
+
+//Del redis data
+func (c *cacheRedis) del(key string) (err error) {
+	_, err = c.client.Do("DEL", key)
+	return err
+}
+
+//Is exist or not
+func (c *cacheRedis) isExist(key string) (flag bool, err error) {
+	flag, err = redis.Bool(c.client.Do("EXISTS", key))
+	return flag, err
+}
+
+//Close redis conn
+func (c *cacheRedis) CloseRedis() {
+	c.client.Close()
+}
